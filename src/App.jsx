cfabd6575933d750878d02765fed0ddf6a429252
phuctrank81 <131422071+phuctrank81@users.noreply.github.com>
2025-10-23 08:40:43 +0700

    update giit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import Hero from "./components/Hero"
 import NavBar from "./components/NavBar"
+import ProductViewer from "./components/ProductViewer"
+import { ScrollTrigger } from "gsap/all"
+import gsap from "gsap"
+gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
     return (
         <main>
             <NavBar />
             <Hero/>
+            <ProductViewer/>
         </main>
     )
 }
