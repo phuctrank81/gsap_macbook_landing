d62c43e2323d33e1dbadc9c28d197788d6b00f81
phuctrank81 <131422071+phuctrank81@users.noreply.github.com>
2025-10-25 08:55:55 +0700

    update to fix

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,25 +1,27 @@
-import clsx from "clsx"
-import useMacbookStore from "../store"
-import { Canvas } from "@react-three/fiber"
-import MacBookModel14 from "./models/Macbook-14"
-import StudioLights from "./three/StudioLights"
-import ModelSwitcher from "./three/ModelSwitcher"
-import { useMediaQuery } from "react-responsive"
+import useMacbookStore from "../store";
+import clsx from "clsx";
+import {Canvas} from "@react-three/fiber";
+import {Box, OrbitControls} from "@react-three/drei";
+import MacbookModel14 from "./models/Macbook-14.jsx";
+
+import StudioLights from "./three/StudioLights.jsx";
+import ModelSwitcher from './three/ModelSwitcher.jsx'
+import {useMediaQuery} from "react-responsive";
 
 const ProductViewer = () => {
+    const { color, scale, setColor, setScale } = useMacbookStore();
 
-    const { color, scale, setColor, setScale } = useMacbookStore()
+    const isMobile = useMediaQuery({ query: '(max-width: 1024px)'});
 
-    const isMobile = useMediaQuery({query: '(max-width: 1024px)'});
     return (
-        <section id='product-viewer'>
+        <section id="product-viewer">
             <h2>Take a closer look.</h2>
 
-            <div className='controls'>
-                <p className='info'>MacbookPro Pro | Available in 14" & 16" in Space Gray & Dark colors  </p>
+            <div className="controls">
+                {/*<p className="info">Macbook Pro | Available in 14" & 16" in Space Gray & Dark colors</p>*/}
 
-                <div className='flex-center gap-5 mt-5'>
-                    <div className='color-control'>
+                <div className="flex-center gap-5 mt-5">
+                    <div className="color-control">
                         <div
                             onClick={() => setColor('#adb5bd')}
                             className={clsx('bg-neutral-300', color === '#adb5bd' && 'active')}
@@ -30,14 +32,13 @@ const ProductViewer = () => {
                         />
                     </div>
 
-                    <div className='size-control'>
+                    <div className="size-control">
                         <div
                             onClick={() => setScale(0.06)}
                             className={clsx(scale === 0.06 ? 'bg-white text-black' : 'bg-transparent text-white')}
                         >
                             <p>14"</p>
                         </div>
-
                         <div
                             onClick={() => setScale(0.08)}
                             className={clsx(scale === 0.08 ? 'bg-white text-black' : 'bg-transparent text-white')}
@@ -48,14 +49,12 @@ const ProductViewer = () => {
                 </div>
             </div>
 
-            <Canvas id="canvas" camera={{position: [0,2,5], fov: 50, near:0.1, far:100}}>
+            <Canvas id="canvas" camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100}}>
                 <StudioLights />
 
-                <ModelSwitcher scale={isMobile ? scale - 0.03 : scale } isMobile = {isMobile}></ModelSwitcher>
-                
+                <ModelSwitcher scale={isMobile ? scale - 0.03 : scale} isMobile={isMobile} />
             </Canvas>
         </section>
     )
 }
-
 export default ProductViewer
--- src/constants/index.js
+++ src/constants/index.js
@@ -27,13 +27,13 @@ const noChangeParts = [
 ];
 
 const performanceImages = [
-    { id: "p1", src: "/performance1.png" },
-    { id: "p2", src: "/performance2.png" },
-    { id: "p3", src: "/performance3.png" },
-    { id: "p4", src: "/performance4.png" },
-    { id: "p5", src: "/performance5.jpg" },
-    { id: "p6", src: "/performance6.png" },
-    { id: "p7", src: "/performance7.png" },
+    { id: "p1", src: "apple-macbook_public/public/performance1.png" },
+    { id: "p2", src: "apple-macbook_public/public/performance2.png" },
+    { id: "p3", src: "apple-macbook_public/public/performance3.png" },
+    { id: "p4", src: "apple-macbook_public/public/performance4.png" },
+    { id: "p5", src: "apple-macbook_public/public/performance5.jpg" },
+    { id: "p6", src: "apple-macbook_public/public/performance6.png" },
+    { id: "p7", src: "apple-macbook_public/public/performance7.png" },
 ];
 
 const performanceImgPositions = [
