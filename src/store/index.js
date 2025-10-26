488ca43d143b4d2530ba33e8fb2551163d0e4bdc
phuctrank81 <131422071+phuctrank81@users.noreply.github.com>
2025-10-26 07:52:17 +0700

    implement features section

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,16 @@
-import { create } from "zustand";
+import { create } from 'zustand';
 
 const useMacbookStore = create((set) => ({
     color: '#2e2c2e',
     setColor: (color) => set({ color }),
 
-    scale:0.08,
+    scale: 0.08,
     setScale: (scale) => set({ scale }),
 
-    reset: () => set({ color: '#2e2c3e', scale: 0.08}),
+    texture: 'apple-macbook_public/public/videos/feature-1.mp4',
+    setTexture: (texture) => set({ texture }),
+
+    reset: () => set({ color: '#2e2c2e', scale: 0.08, texture: 'apple-macbook_public/public/videos/feature-1.mp4' }),
 }))
 
 export default useMacbookStore;
